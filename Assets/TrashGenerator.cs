7e66e43d01c95d2ce57fc9281e7a4774548de364
std777 <b1019159@fun.ac.jp>
2021-11-14 03:57:18 +0900

    add function randomly generate trash on field
    ランダムなゴミをランダムな位置に生成す機能の追加。trashgenerator参照

---
+++ Assets/TrashGenerator.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashGenerator : MonoBehaviour
+{
+    public GameObject TirePrefab;
+    public GameObject Box_1Prefab;
+    public GameObject Box_2Prefab;
+    public GameObject BarrelPrefab;
+    public GameObject Barrel_ClosedPrefab;
+
+    float span = 5.0f; //一秒を1スパンにする
+    float delta = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject item;
+            int dice = Random.Range(1, 6); //1から5までのランダムな数字を選ぶ
+            if(dice == 1)
+            {
+                item = Instantiate(TirePrefab) as GameObject;
+            }
+            else if(dice == 2)
+            {
+                item = Instantiate(Box_1Prefab) as GameObject;
+            }
+            else if (dice == 3)
+            {
+                item = Instantiate(Box_2Prefab) as GameObject;
+            }
+            else if (dice ==4)
+            {
+                item = Instantiate(BarrelPrefab) as GameObject;
+            }
+            else
+            {
+                item = Instantiate(Barrel_ClosedPrefab) as GameObject;
+            }
+
+            float x = new float();
+            float z = new float();
+            x = Random.Range(-90, 91);
+            z = Random.Range(-90, 91);
+            if (x * x + z * z < 10000)
+            {
+                item.transform.position = new Vector3(x, 60, z);
+            }
+            else
+            {
+                return;
+            }
+        }
+        
+    }
+}
