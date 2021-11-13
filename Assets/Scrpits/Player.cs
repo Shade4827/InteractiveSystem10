db6a1827886c41ad7a8d9dcf5ca317fcd939d4eb
Shade4827 <seahorses527@gmail.com>
2021-11-13 21:34:42 +0900

    [Change]移動方法を左右旋回と前進・減速停止に変更 左右移動を旋回に変更 移動速度の上限を設定し、入力が無ければ減速して停止するように変更

--- Assets/Scrpits/Player.cs
+++ Assets/Scrpits/Player.cs
@@ -4,35 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float MOVE_FORCE_MULTIPLIER = 1.0f;
+    float MAX_SPEED = 3.0f;
+    Rigidbody _rb;
+    Vector3 _moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = this.gameObject.GetComponent<Rigidbody>();
+        _moveDirection = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //プレイヤーオブジェクトに力を加えて移動
-        //WASD or 上下左右キーで移動
+        _moveDirection = Vector3.zero;
+
+        //W or 上キーで前に移動
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            this.gameObject.GetComponent<Rigidbody>().AddForce(0,0,1);
+            _moveDirection = this.transform.forward;
         }
-        
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            this.gameObject.GetComponent<Rigidbody>().AddForce(-1,0,0);
-        }
-        
+        _rb.AddForce(MOVE_FORCE_MULTIPLIER * (MAX_SPEED * _moveDirection - _rb.velocity));
+
+        //S or 下キーで停止
         if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            this.gameObject.GetComponent<Rigidbody>().AddForce(0,0,-1);
+            _rb.velocity = Vector3.zero;
+        }
+
+        //AD or 左右キーで左右に旋回
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(new Vector3(0, -1, 0),Space.Self);
         }
 
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            this.gameObject.GetComponent<Rigidbody>().AddForce(1,0,0);
+            this.transform.Rotate(new Vector3(0, 1, 0),Space.Self);
         }
     }
 }
