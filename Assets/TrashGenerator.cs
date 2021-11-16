08f687150a296ba28d4399d2ea13c347335ccf42
std777 <b1019159@fun.ac.jp>
2021-11-16 18:52:30 +0900

    add asset
    assetを追加

--- Assets/TrashGenerator.cs
+++ Assets/TrashGenerator.cs
@@ -9,6 +9,7 @@ public class TrashGenerator : MonoBehaviour
     public GameObject Box_2Prefab;
     public GameObject BarrelPrefab;
     public GameObject Barrel_ClosedPrefab;
+    public GameObject Cola_CanPrefab;
 
     float span = 3.0f; //一秒を1スパンにする
     float delta = 0;
@@ -21,7 +22,7 @@ public class TrashGenerator : MonoBehaviour
         {
             this.delta = 0;
             GameObject item;
-            int dice = Random.Range(1, 6); //1から5までのランダムな数字を選ぶ
+            int dice = Random.Range(1, 7); //1から6までのランダムな数字を選ぶ
             if(dice == 1)
             {
                 item = Instantiate(TirePrefab) as GameObject;
@@ -38,9 +39,13 @@ public class TrashGenerator : MonoBehaviour
             {
                 item = Instantiate(BarrelPrefab) as GameObject;
             }
-            else
+            else if(dice ==5)
             {
                 item = Instantiate(Barrel_ClosedPrefab) as GameObject;
+            }
+            else
+            {
+                item = Instantiate(Cola_CanPrefab) as GameObject;
             }
 
             float x = new float();
