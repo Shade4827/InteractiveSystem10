93898a786e51d04a0629da5cf00ccfa3859d2522
Shade4827 <seahorses527@gmail.com>
2022-01-10 21:35:59 +0900

    [Add]スコアを追加 ・アイテムと手が触れた時にスコアを加算するようにした ・スコアを右上に表示するUIを追加

--- Assets/Scrpits/FingerCollision.cs
+++ Assets/Scrpits/FingerCollision.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class FingerCollision : MonoBehaviour
 {
+    [SerializeField]
+    GameObject gameManager;
+
     void OnCollisionEnter(Collision collision){
         //ゴミに当たったら消す
         if(collision.gameObject.CompareTag("Garbage")){
             GameObject.Destroy(collision.gameObject);
+            gameManager.GetComponent<GameManager>().FlagAddScore();
         }
     }
 }
--- Assets/Scrpits/GameManager.cs
+++ Assets/Scrpits/GameManager.cs
@@ -6,19 +6,27 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    //時間制限
-    //スコア
-    //画面遷移
+    //時間関連
     [SerializeField]
-    float _timeCount = 30.0f;
+    float _timeCount = 100.0f;
     [SerializeField]
     Text _timeText;
+    
+    //スコア関連
+    int _score;
+    bool _flagAddScore;
+    const int ITEM_POINT = 100;
+    [SerializeField]
+    Text _scoreText;
+
+    //画面遷移関連
     const string RESULT_SCENE_NAME = "Result";
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _score = 0;
+        _flagAddScore = false;
     }
 
     // Update is called once per frame
@@ -31,5 +39,19 @@ public class GameManager : MonoBehaviour
         if(_timeCount <= 0.0f){
             SceneManager.LoadScene(RESULT_SCENE_NAME);
         }
+
+        if(_flagAddScore){
+            AddScore();
+            _flagAddScore = false;
+        }
+    }
+
+    public void FlagAddScore(){
+        _flagAddScore = true;
+    }
+    
+    void AddScore(){
+        _score += ITEM_POINT;
+        _scoreText.text = _score.ToString("D7");
     }
 }
